cc33a72f486fc7784647de47eb0fb7a85950f123
liuxicui <870566031@qq.com>
2017-03-04 16:42:00 +0800

    2

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 class About extends React.Component {
   render () {
     return(
-      <div>
+      <div className='about'>
+        <div>
+
+        </div>
         about
       </div>
     )
