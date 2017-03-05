59a66f830ef940c7108b0952f33df66ecda7beb6
liuxicui <870566031@qq.com>
2017-03-05 14:10:18 +0800

    last

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+
 class Search extends React.Component {
   render () {
     return(
       <div>
+        <form>
+          <input type='text' />
+        </form>
         search
       </div>
     )
