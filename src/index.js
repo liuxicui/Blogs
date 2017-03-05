f8d9ea34a7791b8421b3d739adad2ce5def5a007
liuxicui <870566031@qq.com>
2017-03-05 14:34:21 +0800

    fini

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './routes';
-import './main.css';
-import './post.css';
-import 'highlight.js/styles/atom-one-dark.css';
-
-render(<Routers />,document.getElementById('root'));
